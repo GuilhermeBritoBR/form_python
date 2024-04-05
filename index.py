c1feccecf942364f85168f0f0073a2e1c0d38721
Guilherme Santiago de Brito <guilhermesantiagodebritod.s@gmail.com>
2024-04-05 15:10:34 -0300

    formulario continuação

--- index.py
+++ index.py
@@ -1,2 +1,7 @@
-print("hello world!")
-nome = input("Digite seu nome por favor")
+def form():
+    print("Responda o formulário...")
+    nome = input("Digite seu nome:   ")
+    idade = input("Digite sua idade:    ")
+    cidade = input("Digite o nome da sua cidade:   ")
+    país = input("Digite o nome do seu país:    ")
+    print("Este foi seu resultado", nome, ' ', idade, ' ', cidade, ' ', país, '  ', ' Está correto? Digite OK se sim.')
